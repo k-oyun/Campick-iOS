47e61a018b5a7c72b1f1db3d9abeb91c6cf9ce5e
gumraze-git <galaxydh4110@gmail.com>
2025-09-21 13:28:31 +0900

    #86 refactor(auth): 인증 모델 디코딩 공통화

--- campick/Models/Auth/LoginDataDTO.swift
+++ campick/Models/Auth/LoginDataDTO.swift
@@ -3,5 +3,40 @@ import Foundation
 struct LoginDataDTO: Decodable {
     let accessToken: String
     let refreshToken: String?
-}
+    let user: UserDTO?
+    let memberId: String?
+    let dealerId: String?
+    let nickname: String?
+    let profileImageUrl: String?
+    let profileThumbnailUrl: String?
+    let phoneNumber: String?
+    let role: String?
 
+    enum CodingKeys: String, CodingKey {
+        case accessToken
+        case refreshToken
+        case user
+        case memberId
+        case dealerId
+        case profileImageUrl
+        case profileThumbnailUrl
+        case phoneNumber
+        case role
+        case nickname
+    }
+
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        accessToken = try container.decode(String.self, forKey: .accessToken)
+        refreshToken = try container.decodeIfPresent(String.self, forKey: .refreshToken)
+        user = try container.decodeIfPresent(UserDTO.self, forKey: .user)
+
+        memberId = container.decodeFlexibleString(forKey: .memberId)
+        dealerId = container.decodeFlexibleString(forKey: .dealerId)
+        profileImageUrl = container.decodeFlexibleString(forKey: .profileImageUrl)
+        profileThumbnailUrl = container.decodeFlexibleString(forKey: .profileThumbnailUrl)
+        phoneNumber = container.decodeFlexibleString(forKey: .phoneNumber)
+        role = container.decodeFlexibleString(forKey: .role)
+        nickname = container.decodeFlexibleString(forKey: .nickname)
+    }
+}
